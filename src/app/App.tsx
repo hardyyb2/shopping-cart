8ff944bffc3b73f93fb921661768a7f2cb76274a
hardyyb2 <hardikb3732@gmail.com>
2021-02-06 10:21:09 +0530

    [RESTRUCTURED] app module

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import styles from "./App.module.css";
+
+const App: React.FC<{}> = () => {
+  return (
+    <div className={styles.App}>
+      <div>Start </div>
+    </div>
+  );
+};
+
+export default App;
